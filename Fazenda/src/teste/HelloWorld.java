2877c6de43af29fb9bbe2f6a9141832d272b0f77
DiegoLorandi <diego.lorandi@aluno.ifsp.edu.br>
2021-06-25 15:30:46 -0300

    Terceira tentativa de fazer um push para uma branch.

--- Fazenda/src/teste/HelloWorld.java
+++ Fazenda/src/teste/HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld {
 		System.out.println("Connector Java/Mysql instalado");
 		System.out.println("Ciro está conectado também");
 		System.out.println("Colocando código em uma nova branch");
+		System.out.println("terceira tentativa");
 	}
 
 }
