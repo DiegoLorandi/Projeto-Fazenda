f3453b8ba390600a8496945e1924cd5fbbc82d7a
DiegoLorandi <diego.lorandi@aluno.ifsp.edu.br>
2021-06-24 22:38:52 -0300

    Uma linha foi adicionada ao código apenas para testar o funcionamento do commit

--- Fazenda/src/teste/HelloWorld.java
+++ Fazenda/src/teste/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Olá mundo");
+		System.out.println("Connector Java/Mysql instalado");
 	}
 
 }
